3c6baf71e7998a1b86093d58aa35f94d638fcd95
Neelam Dikondwar <neelamdikondwar2001@gmail.com>
2024-08-12 13:04:43 +0530

    Initial commit

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: " ₹0 to 100",
+      array: [0, 100],
+    },
+    {
+      _id: 1,
+      name: " ₹100 to 200",
+      array: [100, 200],
+    },
+    {
+      _id: 2,
+      name: " ₹200 to 500",
+      array: [200, 500],
+    },
+    {
+      _id: 3,
+      name: " ₹500 to 1000",
+      array: [500, 1000],
+    },
+    {
+      _id: 4,
+      name: " ₹1000 to 2000",
+      array: [1000, 2000],
+    },
+    {
+      _id: 4,
+      name: " ₹2000 or more",
+      array: [2000, 9999],
+    },
+  ];
